c73b457fb103dcfbe5e8dd8f27e8fb92d3b86e8f
idkaryadi <karyadi757@gmail.com>
2019-02-15 09:07:58 +0700

    untuk digabung

--- login/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Login
-
-# Register your models here.
-my_model = Login
-admin.site.register(my_model)
--- login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Login(models.Model):
-    nama_akun = models.CharField(max_length = 255)
-    password = models.CharField(max_length = 50)
-    
-    def __str__(self):
-        return self.judul
